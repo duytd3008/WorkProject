0192549d64fddfbee1cde50edb883b51f0ee6e4c
duytd3008 <161331141+duytd3008@users.noreply.github.com>
2024-03-11 22:13:55 +0700

    Update RankStreak(BigQuery).sql

--- SQL/RankStreak(BigQuery).sql
+++ SQL/RankStreak(BigQuery).sql
@@ -2,7 +2,7 @@ WITH TABLE_1 AS (
         SELECT month_M, client_id, nhom_KH, revenue_M1_per_revenue_M  FROM `ad_hoc.IDKH_PercentageRevenue`
         WHERE nhom_KH like 'A%' AND
         month_M BETWEEN (SELECT DATE_SUB(MAX(month_M), INTERVAL 11 month) FROM `ad_hoc.IDKH_PercentageRevenue`)
-        AND (SELECT MAX(month_M) FROM `ghn-cxcscc.ad_hoc.IDKH_PercentageRevenue`)
+        AND (SELECT MAX(month_M) FROM `ad_hoc.IDKH_PercentageRevenue`)
       ),
       TABLE_2 AS (
         SELECT L.*,
